64fc3475cd08fcde833a1f4dd1c6f722580e625c
Erik Teichmann <kontakt.teichmann@gmail.com>
2018-01-30 14:30:18 +0100

    Split implementation from declaration for RayleighyODE

--- include/nonlinear_systems/odes/rayleigh_y_ode.hpp
+++ include/nonlinear_systems/odes/rayleigh_y_ode.hpp
@@ -11,32 +11,38 @@ namespace nonlinear_systems {
         unsigned int N;
         double eta, eps;
 
-
         RayleighYODE(const state_type& frequency, unsigned int N_oscillators, 
-            double nonlinearity, double coupling)
-          : omega(frequency), N(n_oscillator), eta(nonlinearity), 
-          eps(coupling) { };
-
-
-        double CalculateMeanFieldY(const state_type& y) {
-          double sum = 0.;
-          for (unsigned int i = 0; i < N; ++i) {
-            sum += y[2*i+1];
-          }
-          return sum/(double)N;
-        };
-
-
+            double nonlinearity, double coupling);
+        double CalculateMeanFieldY(const state_type& y);
         void operator() (const state_type& y, state_type& dy, 
-            const double t) {
-          double Y = CalculateMeanFieldY(y);
-          for (unsigned int i = 0; i < N; ++i) {
-            dy[2*i] = y[2*i+1];
-            dy[2*i+1] = eta*(1-y[2*i+1]*y[2*i+1])*y[2*i+1] 
-              - omega[i]*omega[i]*y[2*i] + eps*(Y-y[2*i+1]);
-          }
-        };
+            const double t);
     };
 } // nonlinear_systems
 
+using namespace nonlinear_systems;
+
+template<typename state_type>
+RayleighYODE<state_type>::RayleighYODE(const state_type& frequency, 
+    unsigned int N_oscillator, double nonlinearity, double coupling)
+  : omega(frequency), N(N_oscillator), eta(nonlinearity), eps(coupling) {}
+
+template<typename state_type>
+double RayleighYODE<state_type>::CalculateMeanField(const state_type& x) {
+  double sum = 0.;
+  for (unsigned int i = 0; i < N; ++i) {
+    sum += x[2*i+1];
+  }
+  return sum/static_cast<double>(N);
+}
+
+template<typename state_type>
+void RayleighYODE<state_type>::operator()(const state_type& x, state_type& dx,
+    const double t) {
+  double Y = CalculateMeanFieldY(y);
+  for (unsigned int i = 0; i < N; ++i) {
+    dx[2*i] = x[2*i+1];
+    dx[2*i+1] = eta*(1-x[2*i+1]*x[2*i+1])*x[2*i+1] 
+              - omega[i]*omega[i]*x[2*i] + eps*(Y-x[2*i+1]);
+  } 
+}
 #endif
