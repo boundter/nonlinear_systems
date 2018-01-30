ea2e6f8f65fa6335740f790f09f06082d7dee6a0
Erik Teichmann <kontakt.teichmann@gmail.com>
2018-01-30 14:19:11 +0100

    Collapse namespace to nonlinear_systems

--- include/nonlinear_systems/odes/rayleigh_y_ode.hpp
+++ include/nonlinear_systems/odes/rayleigh_y_ode.hpp
@@ -4,41 +4,39 @@
 #include <vector>
 
 namespace nonlinear_systems {
-  namespace ode {
-    template<typename state_type = std::vector<double> >
-      class RayleighYODE {
-        public:
-          state_type omega;
-          unsigned int N;
-          double eta, eps;
+  template<typename state_type = std::vector<double> >
+    class RayleighYODE {
+      public:
+        state_type omega;
+        unsigned int N;
+        double eta, eps;
 
 
-          RayleighYODE(const state_type& frequency, unsigned int N_oscillators, 
-              double nonlinearity, double coupling)
-            : omega(frequency), N(n_oscillator), eta(nonlinearity), 
-            eps(coupling) { };
+        RayleighYODE(const state_type& frequency, unsigned int N_oscillators, 
+            double nonlinearity, double coupling)
+          : omega(frequency), N(n_oscillator), eta(nonlinearity), 
+          eps(coupling) { };
 
 
-          double CalculateMeanFieldY(const state_type& y) {
-            double sum = 0.;
-            for (unsigned int i = 0; i < N; ++i) {
-              sum += y[2*i+1];
-            }
-            return sum/(double)N;
-          };
+        double CalculateMeanFieldY(const state_type& y) {
+          double sum = 0.;
+          for (unsigned int i = 0; i < N; ++i) {
+            sum += y[2*i+1];
+          }
+          return sum/(double)N;
+        };
 
 
-          void operator() (const state_type& y, state_type& dy, 
-              const double t) {
-            double Y = CalculateMeanFieldY(y);
-            for (unsigned int i = 0; i < N; ++i) {
-              dy[2*i] = y[2*i+1];
-              dy[2*i+1] = eta*(1-y[2*i+1]*y[2*i+1])*y[2*i+1] 
-                - omega[i]*omega[i]*y[2*i] + eps*(Y-y[2*i+1]);
-            }
-          };
-      };
-  } // ode
+        void operator() (const state_type& y, state_type& dy, 
+            const double t) {
+          double Y = CalculateMeanFieldY(y);
+          for (unsigned int i = 0; i < N; ++i) {
+            dy[2*i] = y[2*i+1];
+            dy[2*i+1] = eta*(1-y[2*i+1]*y[2*i+1])*y[2*i+1] 
+              - omega[i]*omega[i]*y[2*i] + eps*(Y-y[2*i+1]);
+          }
+        };
+    };
 } // nonlinear_systems
 
 #endif
