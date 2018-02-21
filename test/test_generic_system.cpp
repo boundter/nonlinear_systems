0abea13d0a2858d714315498858d97a236ce1a83
Erik Teichmann <kontakt.teichmann@gmail.com>
2018-02-21 11:28:33 +0100

    Include tests for integration and period with observer

--- test/test_generic_system.cpp
+++ test/test_generic_system.cpp
@@ -8,7 +8,6 @@
 typedef std::vector<double> state_type;
 typedef boost::numeric::odeint::runge_kutta4<state_type> stepper_type;
 
-// TODO: Change boost_check_close_fraction to something absolute
 
 using namespace nonlinear_systems;
 
@@ -40,6 +39,20 @@ class TwoHarmonicOscillators {
       dx[2] = x[3];
       dx[3] = -omega[1]*omega[1]*x[2];
     }
+};
+
+struct PositionAndTimeObserver {
+  std::vector<state_type>& pos;
+  std::vector<double>& ti;
+
+  PositionAndTimeObserver(std::vector<state_type>& position, 
+      std::vector<double>& times)
+    :pos(position), ti(times) {}
+
+  void operator() (const state_type& x, double t) {
+    pos.push_back(x);
+    ti.push_back(t);
+  }
 };
 
 bool CrossedPositiveYAxis(const state_type& previous_state, 
@@ -86,7 +99,7 @@ BOOST_AUTO_TEST_CASE(test_Integrate){
   double t_0 = system.GetTime();
   system.Integrate(dt_time_increase, n_time_increase);
   double t_1 = system.GetTime();
-  BOOST_CHECK_CLOSE(dt_time_increase*static_cast<double>(n_time_increase), 
+  BOOST_CHECK_SMALL(dt_time_increase*static_cast<double>(n_time_increase) - 
       (t_1 - t_0), 0.0001);
 
   // Integrate correctly changes the position
@@ -101,9 +114,36 @@ BOOST_AUTO_TEST_CASE(test_Integrate){
   state_type analytic_solution {sin(t_position), cos(t_position)};
   state_type numeric_solution = system.GetPosition();
   for (size_t i = 0; i < numeric_solution.size(); ++i) {
-    BOOST_CHECK_CLOSE_FRACTION(numeric_solution[i], analytic_solution[i], 0.01);
+    BOOST_CHECK_SMALL(numeric_solution[i] - analytic_solution[i], 0.0001);
   }
 }
+
+
+BOOST_AUTO_TEST_CASE(test_IntegrateObserver){
+  double params = 1.;
+  GenericSystem<HarmonicOscillator> system = 
+    GenericSystem<HarmonicOscillator>(1, 2, &params);
+
+  // Integrate correctly changes the position
+  // Analytic solution is x=A*sin(omega*t + phi)
+  // for x(0)=0 and \dot{x}(0)=1 and with omega=1 A=1 and phi=0
+  double dt = 0.01;
+  unsigned int n = 10;
+  state_type initial_condition {0., 1.};
+  system.SetPosition(initial_condition);
+  std::vector<state_type> position;
+  std::vector<double> times;
+  system.Integrate(dt, n, PositionAndTimeObserver(position, times));
+  for (size_t i = 0; i < times.size(); ++i) {
+    BOOST_CHECK_SMALL(dt*static_cast<double>(i) - times[i], 0.0001);
+    state_type analytic_solution {sin(times[i]), cos(times[i])};
+    state_type numeric_solution = position[i];
+    for (size_t j = 0; j < numeric_solution.size(); ++j) {
+      BOOST_CHECK_SMALL(numeric_solution[j] - analytic_solution[j], 0.0001);
+    }
+  }
+}
+
 
 BOOST_AUTO_TEST_CASE(test_Parameters){
   double params = 1.;
@@ -123,14 +163,15 @@ BOOST_AUTO_TEST_CASE(test_Parameters){
   state_type analytic_solution {1./new_params*sin(new_params*t), cos(new_params*t)};
   state_type numeric_solution = system.GetPosition();
   for (size_t i = 0; i < numeric_solution.size(); ++i) {
-    BOOST_CHECK_CLOSE_FRACTION(numeric_solution[i], analytic_solution[i], 0.01);
+    BOOST_CHECK_SMALL(numeric_solution[i] - analytic_solution[i], 0.0001);
   }
 }
 
+
 BOOST_AUTO_TEST_CASE(test_MeanField){
- double params[2] = {1., 3.};
+  double params[2] = {1., 3.};
   GenericSystem<TwoHarmonicOscillators> system = 
-   GenericSystem<TwoHarmonicOscillators>(2, 2, &params); 
+    GenericSystem<TwoHarmonicOscillators>(2, 2, &params); 
 
   double dt = 0.01;
   unsigned int n = 10000;
@@ -140,18 +181,19 @@ BOOST_AUTO_TEST_CASE(test_MeanField){
   system.Integrate(dt, n);
   state_type numeric_mean_field = system.CalculateMeanField();
   state_type analytic_mean_field {1/2.*(sin(t) + 1./params[1]*sin(params[1]*t)),
-                                  1/2.*(cos(t) + cos(params[1]*t))};
+    1/2.*(cos(t) + cos(params[1]*t))};
   for (size_t i = 0; i < numeric_mean_field.size(); ++i) {
-    BOOST_CHECK_CLOSE_FRACTION(numeric_mean_field[i], analytic_mean_field[i],
-        0.01);
+    BOOST_CHECK_SMALL(numeric_mean_field[i] - analytic_mean_field[i],
+        0.0001);
   }
 }
+
 
 BOOST_AUTO_TEST_CASE(test_CalculatePeriod){
   double params = 1.;
   GenericSystem<HarmonicOscillator> system = 
     GenericSystem<HarmonicOscillator>(1, 2, &params);
-  
+
   // Integrate the system over 5 periods. For the initial conditions 
   // x(0) = 0 and y(0)=1 with omega = 1 we have T = 2*pi
   double dt = 0.01;
@@ -159,16 +201,42 @@ BOOST_AUTO_TEST_CASE(test_CalculatePeriod){
   state_type initial_condition {0., 1.};
   system.SetPosition(initial_condition);
   double T = system.CalculatePeriod(n_average, dt, CrossedPositiveYAxis);
-  BOOST_CHECK_CLOSE_FRACTION(T, 2*M_PI, 0.005);
+  BOOST_CHECK_SMALL(T - 2*M_PI, 0.005);
 
   double linear_T = system.CalculatePeriod(n_average, dt, CrossedPositiveYAxis,
       LinearApprox);
-  BOOST_CHECK_CLOSE_FRACTION(T, 2*M_PI, 0.005);
+  BOOST_CHECK_SMALL(T - 2*M_PI, 0.005);
 
   // Multiple crossings for a harmonic oscillator should lead to a
   // multiplication of the period
   double double_T = system.CalculatePeriod(n_average, dt, CrossedPositiveYAxis,
       2);
-  BOOST_CHECK_CLOSE_FRACTION(double_T, 4*M_PI, 0.005);
+  BOOST_CHECK_SMALL(double_T - 4*M_PI, 0.005);
+}
 
+
+BOOST_AUTO_TEST_CASE(test_CalculatePeriodObserver){
+  double params = 1.;
+  GenericSystem<HarmonicOscillator> system = 
+    GenericSystem<HarmonicOscillator>(1, 2, &params);
+
+  // Integrate the system over 1 period. For the initial conditions 
+  // x(0) = 0 and y(0)=1 with omega = 1 we have T = 2*pi
+  double dt = 0.01;
+  unsigned int n_average = 1;
+  state_type initial_condition {0., 1.};
+  system.SetPosition(initial_condition);
+  std::vector<state_type> position;
+  std::vector<double> times;
+  double T = system.CalculatePeriod(n_average, dt, CrossedPositiveYAxis, 1,
+      PositionAndTimeObserver(position, times));
+  BOOST_CHECK_SMALL(T - 2*M_PI, 0.005);
+  for (size_t i = 0; i < times.size(); ++i) {
+    BOOST_CHECK_SMALL(dt*static_cast<double>(i) - times[i], 0.0001);
+    state_type analytic_solution {sin(times[i]), cos(times[i])};
+    state_type numeric_solution = position[i];
+    for (size_t j = 0; j < numeric_solution.size(); ++j) {
+      BOOST_CHECK_SMALL(numeric_solution[j] - analytic_solution[j], 0.0001);
+    }
+  }
 }
