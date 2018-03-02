69f3d3b4ca350895e68673945df5e6c3d236dc52
Erik Teichmann <kontakt.teichmann@gmail.com>
2018-03-02 17:09:43 +0100

    Split RayleighMeanFieldODE in two seperate classes
    One for coupling in X and one for Y. This makes it easier to integrate a
    coupling to the whole mean-field later on. The differentiation should
    happen in the system, not in the ODE.

--- include/nonlinear_systems/odes/rayleigh_mean_field_ode.hpp
+++ include/nonlinear_systems/odes/rayleigh_mean_field_ode.hpp
@@ -7,49 +7,76 @@
 typedef std::vector<double> state_type;
 
 namespace nonlinear_systems {
-class RayleighMeanFieldODE {
+class RayleighMeanFieldODEX {
   public:
-    RayleighMeanFieldODE(unsigned int N, const state_type& frequency, 
-        double nonlinearity, double coupling, 
-        const char* coupling_coordinate = "y")
-   :_frequency(frequency), _nonlinearity(nonlinearity), _coupling(coupling),
-   _N(N) {
-      if (_frequency.size() != _N) {
-        throw std::length_error("Length of frequency vector and system size N do not match.");
-      } 
-      if (coupling_coordinate == "y") {
-        _offset = 1;
+    RayleighMeanFieldODEX(unsigned int N, const state_type& frequency,
+        double nonlinearity, double coupling)
+      :_frequency(frequency), _nonlinearity(nonlinearity), _coupling(coupling),
+      _N(N) {
+        if (_frequency.size() != _N) {
+          throw std::length_error("Length of frequency vector and system size N do not match.");
+        } 
       }
-      else if (coupling_coordinate == "x") {
-        _offset = 0;
-      }
-      else {
-        throw std::invalid_argument("Did not understand coupling_coordinate. Allowed values are 'x' and 'y'.");
-      }
-    }
 
 
     void operator()(const state_type& x, state_type& dx) {
-      double mean_field = CalculateMeanFieldCoordinate(x);
+      double mean_field = CalculateMeanFieldCoordinateX(x);
       for (unsigned int i = 0; i < _N; ++i) {
         dx[2*i] = x[2*i+1];
         dx[2*i+1] = _nonlinearity*(1-x[2*i+1]*x[2*i+1])*x[2*i+1]
           - _frequency[i]*_frequency[i]*x[2*i]
-          + _coupling*(mean_field - x[2*i+_offset]);
+          + _coupling*(mean_field - x[2*i]);
       }
     }
-  
 
-    protected:
+
+  protected:
     state_type _frequency;
     double _nonlinearity, _coupling;
     unsigned int _N;
-    int _offset;
 
-     double CalculateMeanFieldCoordinate(const state_type& x) {
+    double CalculateMeanFieldCoordinateX(const state_type& x) {
       double sum = 0.;
       for (unsigned int i = 0; i < _N; ++i) {
-        sum += x[2*i+_offset];
+        sum += x[2*i];
+      }
+      return sum/static_cast<double>(_N);
+    }
+};
+
+
+class RayleighMeanFieldODEY {
+  public:
+    RayleighMeanFieldODEY(unsigned int N, const state_type& frequency,
+        double nonlinearity, double coupling)
+      :_frequency(frequency), _nonlinearity(nonlinearity), _coupling(coupling),
+      _N(N) {
+        if (_frequency.size() != _N) {
+          throw std::length_error("Length of frequency vector and system size N do not match.");
+        } 
+      }
+
+
+    void operator()(const state_type& x, state_type& dx) {
+      double mean_field = CalculateMeanFieldCoordinateY(x);
+      for (unsigned int i = 0; i < _N; ++i) {
+        dx[2*i] = x[2*i+1];
+        dx[2*i+1] = _nonlinearity*(1-x[2*i+1]*x[2*i+1])*x[2*i+1]
+          - _frequency[i]*_frequency[i]*x[2*i]
+          + _coupling*(mean_field - x[2*i+1]);
+      }
+    }
+
+
+  protected:
+    state_type _frequency;
+    double _nonlinearity, _coupling;
+    unsigned int _N;
+
+    double CalculateMeanFieldCoordinateY(const state_type& x) {
+      double sum = 0.;
+      for (unsigned int i = 0; i < _N; ++i) {
+        sum += x[2*i+1];
       }
       return sum/static_cast<double>(_N);
     }
--- test/test_rayleigh_mean_field.cpp
+++ test/test_rayleigh_mean_field.cpp
@@ -9,7 +9,8 @@ typedef std::vector<double> state_type;
 
 using namespace nonlinear_systems;
 
-BOOST_AUTO_TEST_CASE(test_ODE_constructor) {
+// TODO: Add Tests for the integration
+BOOST_AUTO_TEST_CASE(test_ODEX_constructor) {
   unsigned int N = 10;
   double nonlinearity = 6.;
   double coupling = 0.134;
@@ -20,15 +21,30 @@ BOOST_AUTO_TEST_CASE(test_ODE_constructor) {
   }
   
   // Test for correct initialization
-  RayleighMeanFieldODE ode = RayleighMeanFieldODE(N, frequency, nonlinearity,
+  RayleighMeanFieldODEX ode = RayleighMeanFieldODEX(N, frequency, nonlinearity,
       coupling);
 
   // Test for wrong length of frequency
-  BOOST_CHECK_THROW(ode = RayleighMeanFieldODE(N+1, frequency, nonlinearity, 
+  BOOST_CHECK_THROW(ode = RayleighMeanFieldODEX(N+1, frequency, nonlinearity, 
         coupling), std::length_error);
+}
 
-  // Test for wrong coupling_coordinate
-  BOOST_CHECK_THROW(ode = RayleighMeanFieldODE(N, frequency, nonlinearity,
-        coupling, "u"), std::invalid_argument);
+
+BOOST_AUTO_TEST_CASE(test_ODEY_constructor) {
+  unsigned int N = 10;
+  double nonlinearity = 6.;
+  double coupling = 0.134;
   
+  state_type frequency;
+  for (unsigned int i = 0; i < N; ++i) {
+    frequency.push_back(1.);
+  }
+  
+  // Test for correct initialization
+  RayleighMeanFieldODEY ode = RayleighMeanFieldODEY(N, frequency, nonlinearity,
+      coupling);
+
+  // Test for wrong length of frequency
+  BOOST_CHECK_THROW(ode = RayleighMeanFieldODEY(N+1, frequency, nonlinearity, 
+        coupling), std::length_error);
 }
