7a815d2105ce604dc00df8be93bea450cb52f15c
Erik Teichmann <kontakt.teichmann@gmail.com>
2018-03-12 10:22:43 +0100

    Rayleigh: Initialize frequency and position

--- include/nonlinear_systems/systems/rayleigh_mean_field_system.hpp
+++ include/nonlinear_systems/systems/rayleigh_mean_field_system.hpp
@@ -1,4 +1,9 @@
+#ifndef __RAYLEIGH_MEAN_FIELD_SYSTEM__
+#define __RAYLEIGH_MEAN_FIELD_SYSTEM__
+
 #include <vector>
+#include <random>
+#include <functional>
 #include <nonlinear_systems/odes/rayleigh_mean_field_ode.hpp>
 #include <nonlinear_systems/systems/generic_system.hpp>
 
@@ -10,12 +15,25 @@ namespace nonlinear_systems {
     boost::numeric::odeint::runge_kutta4<state_type>>  {
       public:
         RayleighMeanFieldSystem(unsigned int N, double nonlinearity, 
-            double coupling, const char* coupling_coordinate = "y")
+            double coupling, const char* coupling_coordinate = "y",
+            unsigned long int seed=123456789)
           :GenericSystem(N, 2){
-            state_type frequency;
-            for (unsigned int i = 0; i < N; ++i) {
-              frequency.push_back(1);
-            }
+            std::mt19937_64 rng(seed);
+
+            double min_x = -3., max_x = 3.;
+            std::uniform_real_distribution<double> uniform(min_x, max_x);
+            std::function<double()> uniform_dist = std::bind(uniform,
+                std::ref(rng));
+            x = SampleDistribution(x.size(), &uniform_dist);
+            
+            frequency.resize(N);
+            double mean_frequency = 1., stdev_frequency = 0.01;
+            std::normal_distribution<double> normal(mean_frequency, 
+                stdev_frequency);
+            std::function<double()> normal_dist = std::bind(normal,
+                std::ref(rng));
+            frequency = SampleDistribution(frequency.size(), &normal_dist);
+            
             _coupling_coordinate = coupling_coordinate;
             if(_coupling_coordinate == "x") {
               ode = new RayleighMeanFieldODEX(N, frequency, nonlinearity, 
@@ -30,8 +48,26 @@ namespace nonlinear_systems {
             }
           }
 
+
+        state_type GetFrequency() {
+          return frequency;
+        } 
+
       protected:
         const char* _coupling_coordinate;
+        state_type frequency;
+
+
+        state_type SampleDistribution(size_t number_samples, 
+            std::function<double()>* distribution) {
+          state_type samples;
+          for (size_t i = 0; i < number_samples; ++i) {
+            samples.push_back((*distribution)());
+          }
+          return samples;
+        }
     };
 
 } // nonlinear_systems
+
+#endif
--- test/test_rayleigh_mean_field.cpp
+++ test/test_rayleigh_mean_field.cpp
@@ -1,9 +1,10 @@
 #define BOOST_TEST_MODULE RayleighMeanFieldSystem
 #include <boost/test/included/unit_test.hpp>
-#include <nonlinear_systems/odes/rayleigh_mean_field_ode.hpp>
-#include <nonlinear_systems/systems/rayleigh_mean_field_system.hpp>
 #include <stdexcept>
 #include <cmath>
+#include <random>
+#include <nonlinear_systems/odes/rayleigh_mean_field_ode.hpp>
+#include <nonlinear_systems/systems/rayleigh_mean_field_system.hpp>
 
 typedef std::vector<double> state_type;
 
@@ -47,7 +48,6 @@ BOOST_AUTO_TEST_CASE(test_ODEY_constructor) {
       std::length_error);
 }
 
-
 BOOST_AUTO_TEST_CASE(test_system_constructor) {
   unsigned int N = 10;
   double nonlinearity = 6.;
@@ -65,4 +65,26 @@ BOOST_AUTO_TEST_CASE(test_system_constructor) {
   // Test constructor for invalid coupling
   BOOST_CHECK_THROW(RayleighMeanFieldSystem(N, nonlinearity, coupling, "u"),
       std::invalid_argument);
+
+  // Check initialization of frequency and position from mt19937_64
+  unsigned long int seed = 123456789;
+  double min_value = -3.;
+  double max_value = 3.;
+  double mean = 1.;
+  double stdev = 0.01;
+  std::mt19937_64 rng(seed);
+  std::uniform_real_distribution<double> uniform(min_value, max_value);
+  std::normal_distribution<double> normal(mean, stdev);
+  state_type position, frequency;
+  for (unsigned int i = 0; i < 2*N; ++i) {
+    position.push_back(uniform(rng));
+  }
+  for (unsigned int i = 0; i < N; ++i) {
+    frequency.push_back(normal(rng));
+  }
+  RayleighMeanFieldSystem system = RayleighMeanFieldSystem(N, nonlinearity,
+      coupling, "y", seed);
+  BOOST_TEST(system.GetPosition() == position, boost::test_tools::per_element());
+  BOOST_TEST(system.GetFrequency() == frequency, boost::test_tools::per_element());
+
 }
