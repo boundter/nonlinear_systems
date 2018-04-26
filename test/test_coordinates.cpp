edbcd5409fd267f14cec21ab5ea3275b5d9440d2
Erik Teichmann <kontakt.teichmann@gmail.com>
2018-04-26 11:24:20 +0200

    Cleanup Coordinates test

--- test/test_coordinates.cpp
+++ test/test_coordinates.cpp
@@ -1,28 +1,30 @@
 #define BOOST_TEST_MODULE Coordinates
 #include <boost/test/included/unit_test.hpp>
-#include <nonlinear_systems/misc/coordinates.hpp>
 #include <vector>
+#include <nonlinear_systems/misc/coordinates.hpp>
 
 typedef std::vector<double> state_type;
 
 using namespace nonlinear_systems;
 
-BOOST_AUTO_TEST_CASE(test_spherical) {
-  // 2-dimensions
-  state_type x_2 = {2., 3.};
-  state_type spherical_2 = {3.606, 0.9828};
-  state_type spherical = CartesianToSpherical(x_2);
-  BOOST_TEST(spherical.size() == x_2.size());
+
+BOOST_AUTO_TEST_CASE(CartesianToSpherical2D) {
+  state_type x = {2., 3.};
+  state_type analytical = {3.606, 0.9828};
+  state_type spherical = CartesianToSpherical(x);
+  BOOST_REQUIRE_EQUAL(spherical.size(), x.size());
   for (size_t i = 0; i < spherical.size(); ++i) {
-    BOOST_CHECK_CLOSE_FRACTION(spherical[i], spherical_2[i], 0.01);
+    BOOST_CHECK_CLOSE(spherical[i], analytical[i], 0.1);
   }
-  
-  // 3-dimensions
-  state_type x_3 = {6., 2., -1.};
-  state_type spherical_3 = {6.403, 0.3567, -0.4636};
-  spherical = CartesianToSpherical(x_3);
-  BOOST_TEST(spherical.size() == x_3.size());
+}
+
+
+BOOST_AUTO_TEST_CASE(CartesianToSpherical3D) {
+  state_type x = {6., 2., -1.};
+  state_type analytical = {6.403, 0.3567, -0.4636};
+  state_type spherical = CartesianToSpherical(x);
+  BOOST_REQUIRE_EQUAL(spherical.size(), x.size());
   for (size_t i = 0; i < spherical.size(); ++i) {
-    BOOST_CHECK_CLOSE_FRACTION(spherical[i], spherical_3[i], 0.01);
+    BOOST_CHECK_CLOSE(spherical[i], analytical[i], 0.1);
   }
 }
