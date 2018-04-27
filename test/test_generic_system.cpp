3aba47ab434748189d4b044a8b16f98df9044349
Erik Teichmann <kontakt.teichmann@gmail.com>
2018-04-27 11:52:47 +0200

    Cleanup first half of tests for GenericSystem

--- test/test_generic_system.cpp
+++ test/test_generic_system.cpp
@@ -1,29 +1,17 @@
 #define BOOST_TEST_MODULE GenericSystem
 #include <boost/test/included/unit_test.hpp>
-#include <nonlinear_systems/systems/generic_system.hpp>
-#include <stdexcept>
 #include <cmath>
-#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <boost/numeric/odeint/stepper/runge_kutta4.hpp>
+#include <nonlinear_systems/odes/harmonic_oscillator_ode.hpp>
+#include <nonlinear_systems/systems/generic_system.hpp>
 
 typedef std::vector<double> state_type;
 typedef boost::numeric::odeint::runge_kutta4<state_type> stepper_type;
 
 
 using namespace nonlinear_systems;
-
-class HarmonicOscillator {
-  public:
-    double omega;
-
-    HarmonicOscillator(void* params) {
-      omega = reinterpret_cast<double*>(params)[0];
-    }
-
-    void operator()(const state_type& x, state_type& dx, const double t) {
-      dx[0] = x[1];
-      dx[1] = -omega*omega*x[0]; 
-    }
-};
 
 class TwoHarmonicOscillators {
   public:
@@ -68,82 +56,99 @@ double LinearApprox(const state_type& previous_state, double previous_time,
 }
 
 
-BOOST_AUTO_TEST_CASE(test_Position) {
-  double params[] = {1.};
-  GenericSystem<HarmonicOscillator> system = 
-    GenericSystem<HarmonicOscillator>(1, 2, params);
+struct Harmonic {
+  Harmonic() {
+    params[0] = 2.; 
+    N = 1;
+    dimension = 2;
+    system = std::shared_ptr<GenericSystem<HarmonicOscillatorODE> > (
+        new GenericSystem<HarmonicOscillatorODE>(N, dimension, params));
+  }
 
+  ~Harmonic() {;}
+
+  double params[1];
+  unsigned int N;
+  unsigned int dimension;
+  std::shared_ptr<GenericSystem<HarmonicOscillatorODE> > system;
+};
+
+
+BOOST_FIXTURE_TEST_CASE(position, Harmonic) {
   // state initializes to zero
-  state_type zero {0., 0.};
-  BOOST_TEST(system.GetPosition() == zero);
+  state_type position = system->GetPosition();
+  BOOST_REQUIRE_EQUAL(position.size(), dimension);
+  BOOST_CHECK_SMALL(position[0], 0.01);
+  BOOST_CHECK_SMALL(position[1], 0.01);
 
   // SetPosition correctly sets the internal variable
   state_type new_state {0.5, 0.1};
-  system.SetPosition(new_state);
-  BOOST_TEST(system.GetPosition() == new_state);
+  system->SetPosition(new_state);
+  position = system->GetPosition();
+  BOOST_REQUIRE_EQUAL(position.size(), new_state.size());
+  BOOST_CHECK_CLOSE(position[0], new_state[0], 0.01);
+  BOOST_CHECK_CLOSE(position[1], new_state[1], 0.01);
 
   // setting the position to a wrong size returns an error
-  state_type too_long {0.3, 0.1, 6.0};
-  BOOST_CHECK_THROW(system.SetPosition(too_long), std::length_error);
+  state_type too_long = {0.3, 0.1, 6.0};
+  state_type too_short = {0.1};
+  BOOST_CHECK_THROW(system->SetPosition(too_long), std::length_error);
+  BOOST_CHECK_THROW(system->SetPosition(too_short), std::length_error);
 }
 
 
-BOOST_AUTO_TEST_CASE(test_resize) {
-  double params[] = {1.};
-  GenericSystem<HarmonicOscillator> system(1, 2, params);
-
-  system.Resize(2);
+BOOST_FIXTURE_TEST_CASE(resize_method, Harmonic) {
+  system->Resize(2);
 
   // check if state has correct size
-  BOOST_TEST(system.GetPosition().size() == 4);
+  BOOST_REQUIRE_EQUAL(system->GetPosition().size(), 4);
 
   // check if new state can be set
-  state_type new_state {0., 0., 1., 1.};
-  BOOST_CHECK_NO_THROW(system.SetPosition(new_state));
+  state_type new_state = {1., 2., 3., 4.};
+  BOOST_CHECK_NO_THROW(system->SetPosition(new_state));
+  state_type position = system->GetPosition();
+  BOOST_REQUIRE_EQUAL(position.size(), new_state.size());
+  BOOST_CHECK_CLOSE(position[0], new_state[0], 0.01);
+  BOOST_CHECK_CLOSE(position[1], new_state[1], 0.01);
+  BOOST_CHECK_CLOSE(position[2], new_state[2], 0.01);
+  BOOST_CHECK_CLOSE(position[3], new_state[3], 0.01);
 
   // check false lengths
-  state_type too_short {0., 0., 1.};
-  state_type too_long {0., 0., 1., 1., 2.};
-  BOOST_CHECK_THROW(system.SetPosition(too_short), std::length_error);
-  BOOST_CHECK_THROW(system.SetPosition(too_long), std::length_error);
+  state_type too_short(3);
+  state_type too_long(5);
+  BOOST_CHECK_THROW(system->SetPosition(too_short), std::length_error);
+  BOOST_CHECK_THROW(system->SetPosition(too_long), std::length_error);
 }
 
 
-BOOST_AUTO_TEST_CASE(test_Integrate){
-  double params = 1.;
-  GenericSystem<HarmonicOscillator> system = 
-    GenericSystem<HarmonicOscillator>(1, 2, &params);
-
-  // Integrate increases time correctly
-  double dt_time_increase = 0.01;
-  unsigned int n_time_increase = 10000;
-  double t_0 = system.GetTime();
-  system.Integrate(dt_time_increase, n_time_increase);
-  double t_1 = system.GetTime();
-  BOOST_CHECK_SMALL(dt_time_increase*static_cast<double>(n_time_increase) - 
-      (t_1 - t_0), 0.0001);
-
+BOOST_FIXTURE_TEST_CASE(integrate, Harmonic){
   // Integrate correctly changes the position
   // Analytic solution is x=A*sin(omega*t + phi)
-  // for x(0)=0 and \dot{x}(0)=1 and with omega=1 A=1 and phi=0
-  double dt_position = 0.01;
-  unsigned int n_position = 10000;
-  double t_position = dt_position*static_cast<double>(n_position);
+  // for x(0)=0 and \dot{x}(0)=1 and with omega=2
+  // the solution is x = 0.5*sin(omega*t), \dot{x} = cos(omega*t)
+  double dt = 0.01;
+  unsigned int n = 10000;
+  double t = dt*static_cast<double>(n);
   state_type initial_condition {0., 1.};
-  system.SetPosition(initial_condition);
-  system.Integrate(dt_position, n_position);
-  state_type analytic_solution {sin(t_position), cos(t_position)};
-  state_type numeric_solution = system.GetPosition();
-  for (size_t i = 0; i < numeric_solution.size(); ++i) {
-    BOOST_CHECK_SMALL(numeric_solution[i] - analytic_solution[i], 0.0001);
-  }
+  system->SetPosition(initial_condition);
+  double t_0 = system->GetTime();
+  system->Integrate(dt, n);
+  state_type analytical = {1./params[0]*sin(params[0]*t), cos(params[0]*t)};
+  state_type numerical = system->GetPosition();
+  BOOST_REQUIRE_EQUAL(numerical.size(), analytical.size());
+  BOOST_CHECK_CLOSE(numerical[0], analytical[0], 0.01);
+  BOOST_CHECK_CLOSE(numerical[1], analytical[1], 0.01);
+  
+  // Integrate increases time correctly
+  double t_1 = system->GetTime();
+  BOOST_CHECK_CLOSE(t, (t_1 - t_0), 0.000001);
 }
 
 
 BOOST_AUTO_TEST_CASE(test_IntegrateObserver){
   double params = 1.;
-  GenericSystem<HarmonicOscillator> system = 
-    GenericSystem<HarmonicOscillator>(1, 2, &params);
+  GenericSystem<HarmonicOscillatorODE> system = 
+    GenericSystem<HarmonicOscillatorODE>(1, 2, &params);
 
   // Integrate correctly changes the position
   // Analytic solution is x=A*sin(omega*t + phi)
@@ -168,8 +173,8 @@ BOOST_AUTO_TEST_CASE(test_IntegrateObserver){
 
 BOOST_AUTO_TEST_CASE(test_Parameters){
   double params = 1.;
-  GenericSystem<HarmonicOscillator> system =
-    GenericSystem<HarmonicOscillator>(1, 2, &params);
+  GenericSystem<HarmonicOscillatorODE> system =
+    GenericSystem<HarmonicOscillatorODE>(1, 2, &params);
 
   // Integrate a given time with new parameters and check closeness
   // if everything is like before, but with omega =/= 1, then A = 1/omega
@@ -214,7 +219,7 @@ BOOST_AUTO_TEST_CASE(test_MeanFieldSpherical){
   double params = 1.;
 
   // 1 dimensional; ODE will not be used, HarmonicOscillator as dummy
-  GenericSystem<HarmonicOscillator> system_1(5, 1, &params);
+  GenericSystem<HarmonicOscillatorODE> system_1(5, 1, &params);
   state_type x_1 = {0., 0., 3*M_PI, 3*M_PI, M_PI/2.};
   system_1.SetPosition(x_1);
   state_type spherical_1 = system_1.CalculateMeanFieldSpherical();
@@ -223,7 +228,7 @@ BOOST_AUTO_TEST_CASE(test_MeanFieldSpherical){
   BOOST_CHECK_CLOSE(spherical_1[1], M_PI/2., 0.01);
   
   // 3 dimensional; ODE will not be used, HarmonicOscillator as dummy
-  GenericSystem<HarmonicOscillator> system_2(4, 2, &params);
+  GenericSystem<HarmonicOscillatorODE> system_2(4, 2, &params);
   state_type x_2 = {0., 5., 3., 2., 1., 3., 7., 8.};
   system_2.SetPosition(x_2);
   state_type spherical_2 = system_2.CalculateMeanFieldSpherical();
@@ -232,7 +237,7 @@ BOOST_AUTO_TEST_CASE(test_MeanFieldSpherical){
   BOOST_CHECK_CLOSE(spherical_2[1], 1.0222, 0.1);
   
   // 3 dimensional; ODE will not be used, HarmonicOscillator as dummy
-  GenericSystem<HarmonicOscillator> system_3(3, 3, &params);
+  GenericSystem<HarmonicOscillatorODE> system_3(3, 3, &params);
   state_type x_3 = {0., 1., 5., 4., 2., 7., 5., 2., 4.};
   system_3.SetPosition(x_3);
   state_type spherical_3 = system_3.CalculateMeanFieldSpherical();
@@ -246,8 +251,8 @@ BOOST_AUTO_TEST_CASE(test_MeanFieldSpherical){
 
 BOOST_AUTO_TEST_CASE(test_CalculatePeriod){
   double params = 1.;
-  GenericSystem<HarmonicOscillator> system = 
-    GenericSystem<HarmonicOscillator>(1, 2, &params);
+  GenericSystem<HarmonicOscillatorODE> system = 
+    GenericSystem<HarmonicOscillatorODE>(1, 2, &params);
 
   // Integrate the system over 5 periods. For the initial conditions 
   // x(0) = 0 and y(0)=1 with omega = 1 we have T = 2*pi
@@ -272,8 +277,8 @@ BOOST_AUTO_TEST_CASE(test_CalculatePeriod){
 
 BOOST_AUTO_TEST_CASE(test_CalculatePeriodObserver){
   double params = 1.;
-  GenericSystem<HarmonicOscillator> system = 
-    GenericSystem<HarmonicOscillator>(1, 2, &params);
+  GenericSystem<HarmonicOscillatorODE> system = 
+    GenericSystem<HarmonicOscillatorODE>(1, 2, &params);
 
   // Integrate the system over 1 period. For the initial conditions 
   // x(0) = 0 and y(0)=1 with omega = 1 we have T = 2*pi
