2d8fbd10fb8a6b4238ffaf351164080dc9f8356f
Erik Teichmann <kontakt.teichmann@gmail.com>
2018-04-27 13:32:44 +0200

    Add initial version of KuramotoSakaguchiODE

---
+++ include/nonlinear_systems/odes/kuramoto_sakaguchi_ode.hpp
@@ -0,0 +1,51 @@
+#ifndef __KURAMOTO_SAKAGUCHI_ODE__
+#define __KURAMOTO_SAKAGUCHI_ODE__
+
+#include <cmath>
+#include <vector>
+
+typedef std::vector<double> state_type;
+
+namespace nonlinear_systems {
+// TODO: test
+// TODO: document
+class KuramotoSakaguchiODE {
+  public:
+    unsigned int _N;
+    state_type _frequency;
+    double _coupling;
+    double _phase_shift;
+
+    KuramotoSakaguchiODE(unsigned int N, state_type frequency, double coupling, 
+        double phase_shift)
+   :_N(N), _frequency(frequency), _coupling(coupling), _phase_shift(phase_shift) {
+      // TODO: Check length
+      ;
+    }
+
+
+    void operator()(const state_type& x, state_type& dx, double t) {
+      state_type mean_field = CalculateMeanField(x);
+      for (unsigned int i = 0; i < _N; ++i) {
+        dx[i] = _frequency[i] + _coupling*mean_field[0]
+                                *sin(mean_field[1] - x[i] + phase_shift);
+      }
+    }
+
+
+    state_type CalculateMeanField(const state_type& x) {
+      state_type mean_field(2);
+      double re = 0., im = 0.;
+      for (unsigned int i = 0; i < _N; ++i) {
+        re += cos(x[i]);
+        im += sin(x[i]);
+      }
+      re /= static_cast<double>(_N);
+      im /= static_cast<double>(_N);
+      mean_field[0] = sqrt(re*re + im*im);
+      mean_field[1] = atan2(im, re);
+    }
+};
+} // nonlinear_systems
+
+#endif
