bacc6a17c7a9089c49d57ffa25a43c5104b8cb63
Erik Teichmann <kontakt.teichmann@gmail.com>
2018-04-27 12:02:39 +0200

    Add observer for the position

---
+++ include/nonlinear_systems/observers/position_observer.hpp
@@ -0,0 +1,25 @@
+#ifndef __POSITION_OBSERVER__
+#define __POSITION_OBSERVER__
+
+#include <vector>
+
+namespace nonlinear_systems{
+/*!
+ *  Observe s the position of the system and saves it together with the time.
+ */
+template<typename state_type = std::vector<double> >
+struct PositionObserver {
+  std::vector<state_type>& _x;
+  std::vector<double>& _t;
+
+  PositionObserver(std::vector<state_type>& position, std::vector<double>* t)
+    : _x(position), _t(t) {;}
+
+  void operator()(const state_type& x, double t) {
+    _x.push_back(x);
+    _t.push_back(t);
+  }
+};
+} // nonlinear_systems
+
+#endif
