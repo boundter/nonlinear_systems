1d218883b1ce72be6eb30e0c861049e52098e8d9
Erik Teichmann <kontakt.teichmann@gmail.com>
2018-04-30 16:41:38 +0200

    Cleanup tests for GenericNetwork

--- test/test_generic_network.cpp
+++ test/test_generic_network.cpp
@@ -1,6 +1,8 @@
 #define BOOST_TEST_MODULE GenericNetwork
-#include <cmath>
 #include <boost/test/included/unit_test.hpp>
+#include <cmath>
+#include <vector>
+#include <nonlinear_systems/odes/harmonic_oscillator_ode.hpp>
 #include <nonlinear_systems/systems/generic_network.hpp>
 
 typedef std::vector<double> state_type;
@@ -8,20 +10,7 @@ typedef std::vector<std::vector<double> > network_type;
 
 using namespace nonlinear_systems;
 
-class HarmonicOscillator {
-  public:
-    double omega;
-
-    HarmonicOscillator(void* params) {
-      omega = reinterpret_cast<double*>(params)[0];
-    }
-
-    void operator()(const state_type& x, state_type& dx, const double t) {
-      dx[0] = x[1];
-      dx[1] = -omega*omega*x[0]; 
-    }
-};
-
+// TODO: Test constructor?
 
 class TwoHarmonicOscillators {
   public:
@@ -40,44 +29,68 @@ class TwoHarmonicOscillators {
 };
 
 
-BOOST_AUTO_TEST_CASE(test_conversion_state_matrix) {
-  std::vector<unsigned int> node_sizes = {3, 2};
-  double params = 1.;
-  GenericNetwork<HarmonicOscillator, int> network(node_sizes, 1, &params);
+struct F {
+  F() {
+    node_sizes = {3, 2};
+    params = 1.;
+    // Harmonic Oscillator is just a dummy
+    network = std::unique_ptr<GenericNetwork<HarmonicOscillatorODE> > (
+        new GenericNetwork<HarmonicOscillatorODE>(node_sizes, 2, &params));
+  }
 
-  // set a new state
-  std::vector<int> x = {1, 2, 3, 4, 5};
-  BOOST_CHECK_NO_THROW(network.SetState(x));
+  std::vector<unsigned int> node_sizes;
+  double params;
+  std::unique_ptr<GenericNetwork<HarmonicOscillatorODE> > network;
+};  
+
+
+BOOST_FIXTURE_TEST_CASE(conversion_state, F) {
+  // set a new state of the correct length
+  state_type x = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  BOOST_CHECK_NO_THROW(network->SetState(x));
   
   // state was correctly set
-  std::vector<int> state = network.GetPosition();
-  BOOST_TEST(state == x);
+  state_type state = network->GetPosition();
+  BOOST_REQUIRE_EQUAL(state.size(), x.size());
+  BOOST_CHECK_CLOSE(state[0], x[0], 0.01);
+  BOOST_CHECK_CLOSE(state[1], x[1], 0.01);
+  BOOST_CHECK_CLOSE(state[2], x[2], 0.01);
+  BOOST_CHECK_CLOSE(state[3], x[3], 0.01);
+  BOOST_CHECK_CLOSE(state[4], x[4], 0.01);
+  BOOST_CHECK_CLOSE(state[5], x[5], 0.01);
+  BOOST_CHECK_CLOSE(state[6], x[6], 0.01);
+  BOOST_CHECK_CLOSE(state[7], x[7], 0.01);
+  BOOST_CHECK_CLOSE(state[8], x[8], 0.01);
+  BOOST_CHECK_CLOSE(state[9], x[9], 0.01);
   
   // correct node indices
-  std::vector<unsigned int> node_indices = {0, 3, 5};
-  BOOST_TEST(node_indices == network.GetNodeIndices());
+  std::vector<unsigned int> node_indices = {0, 6, 10};
+  std::vector<unsigned int> indices = network->GetNodeIndices();
+  BOOST_REQUIRE_EQUAL(indices.size(), node_indices.size());
+  BOOST_CHECK_EQUAL(indices[0], node_indices[0]);
+  BOOST_CHECK_EQUAL(indices[1], node_indices[1]);
+  BOOST_CHECK_EQUAL(indices[2], node_indices[2]);
 
   // output to nodes works correct
-  std::vector<std::vector<int> > nodes = network.GetNodes();
-  BOOST_TEST(x[0] == nodes[0][0]);
-  BOOST_TEST(x[1] == nodes[0][1]);
-  BOOST_TEST(x[2] == nodes[0][2]);
-  BOOST_TEST(x[3] == nodes[1][0]);
-  BOOST_TEST(x[4] == nodes[1][1]);
-}
-
-
-BOOST_AUTO_TEST_CASE(test_time) {
-  std::vector<unsigned int> node_sizes = {3, 2};
-  double params = 1.;
-  GenericNetwork<HarmonicOscillator, int> network(node_sizes, 1, &params);
-
-  BOOST_CHECK_SMALL(network.GetTime(), 0.01);
+  network_type nodes = network->GetNodes();
+  BOOST_REQUIRE_EQUAL(nodes.size(), 2);
+  BOOST_REQUIRE_EQUAL(nodes[0].size(), 6);
+  BOOST_REQUIRE_EQUAL(nodes[1].size(), 4);
+  BOOST_CHECK_CLOSE(nodes[0][0], x[0], 0.01);
+  BOOST_CHECK_CLOSE(nodes[0][1], x[1], 0.01);
+  BOOST_CHECK_CLOSE(nodes[0][2], x[2], 0.01);
+  BOOST_CHECK_CLOSE(nodes[0][3], x[3], 0.01);
+  BOOST_CHECK_CLOSE(nodes[0][4], x[4], 0.01);
+  BOOST_CHECK_CLOSE(nodes[0][5], x[5], 0.01);
+  BOOST_CHECK_CLOSE(nodes[1][0], x[6], 0.01);
+  BOOST_CHECK_CLOSE(nodes[1][1], x[7], 0.01);
+  BOOST_CHECK_CLOSE(nodes[1][2], x[8], 0.01);
+  BOOST_CHECK_CLOSE(nodes[1][3], x[9], 0.01);
 }
 
 
 // TODO: Maybe also test with observer?
-BOOST_AUTO_TEST_CASE(test_integrate) {
+BOOST_AUTO_TEST_CASE(integrate) {
   std::vector<unsigned int> node_sizes = {1, 1};
   double params = 1.;
   GenericNetwork<TwoHarmonicOscillators> network(node_sizes, 2, &params);
@@ -110,17 +123,15 @@ BOOST_AUTO_TEST_CASE(test_integrate) {
 }
 
 
-BOOST_AUTO_TEST_CASE(test_mean_field) {
-  std::vector<unsigned int> node_sizes = {3, 2};
-  double params = 1;
-  GenericNetwork<HarmonicOscillator> network(node_sizes, 2, &params);
-
+BOOST_FIXTURE_TEST_CASE(mean_field, F) {
   state_type state = {1., 3., 2., 4., 3., 5., 1., 3., 2., 4.};
   state_type mean_field_1 = {2., 4.};
   state_type mean_field_2 = {1.5, 3.5};
-  network.SetState(state);
-  network_type mean_field = network.CalculateMeanField();
-  
+  network->SetState(state);
+  network_type mean_field = network->CalculateMeanField();
+  BOOST_REQUIRE_EQUAL(mean_field.size(), 2);
+  BOOST_REQUIRE_EQUAL(mean_field[0].size(), 2);
+  BOOST_REQUIRE_EQUAL(mean_field[1].size(), 2);
   BOOST_CHECK_CLOSE(mean_field[0][0], mean_field_1[0], 0.01);
   BOOST_CHECK_CLOSE(mean_field[0][1], mean_field_1[1], 0.01);
   BOOST_CHECK_CLOSE(mean_field[1][0], mean_field_2[0], 0.01);
