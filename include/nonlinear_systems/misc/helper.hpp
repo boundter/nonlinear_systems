298cfbc62a5f88d062408178a857a169cecdcedc
Erik Teichmann <kontakt.teichmann@gmail.com>
2018-05-02 17:29:50 +0200

    Refactor MeanFieldHelper in separate file

---
+++ include/nonlinear_systems/misc/helper.hpp
@@ -0,0 +1,53 @@
+#ifndef __HELPER__
+#define __HELPER__
+
+#include <vector>
+
+namespace nonlinear_systems {
+// This helper class allows the extraction of the mean field for the calculation
+// of the average frequency, this way one observer can handle both general
+// systems and networks.
+template <typename mean_field_type>
+class MeanFieldHelper {
+  public:
+
+    std::vector<double> GetMeanFieldPhase(const mean_field_type& mean_field) {
+      std::vector<double> phase;
+      for (size_t i = 1; i < mean_field.size(); ++i) {
+        phase.push_back(mean_field[i]);
+      }
+      return phase;
+    }
+
+
+    std::vector<double> GetOrderParameter(const mean_field_type& mean_field) {
+      std::vector<double> order_parameter = {mean_field[0]};
+      return order_parameter;
+    }
+};
+
+
+// Template specialization for networks.
+template<> std::vector<double> MeanFieldHelper<std::vector<std::vector<double> > >
+::GetMeanFieldPhase(const std::vector<std::vector<double> >& mean_field) {
+  std::vector<double> phase;
+  for (unsigned int i = 0; i < mean_field.size(); ++i) {
+    for (size_t j = 1; j < mean_field[i].size(); ++j) {
+      phase.push_back(mean_field[i][j]);
+    }
+  }
+  return phase;
+}
+
+
+template<> std::vector<double> MeanFieldHelper<std::vector<std::vector<double> > >
+::GetOrderParameter(const std::vector<std::vector<double> >& mean_field) {
+  std::vector<double> order_parameter;
+  for (size_t i = 0; i < mean_field.size(); ++i) {
+    order_parameter.push_back(mean_field[i][0]);
+  }
+  return order_parameter;
+}
+} // nonlinear_systems
+
+#endif
--- include/nonlinear_systems/observers/average_frequency_observer.hpp
+++ include/nonlinear_systems/observers/average_frequency_observer.hpp
@@ -4,6 +4,7 @@
 #include <cmath> // copysign, atan2, fabs
 #include <vector>
 #include <nonlinear_systems/observers/statistics_observer.hpp>
+#include <nonlinear_systems/misc/helper.hpp>
 
 // TODO: Add observer for AverageFrequencyMeanFieldObserver
 // TODO: Add observer for AverageFrequencyMeanFieldPhaseAndPhaseObserver
@@ -116,37 +117,6 @@ class AverageFrequencyObserver {
       return phases;
     }
 };
-
-
-// TODO: Refactor in speerate file
-// This helper class allows the extraction of the mean field for the calculation
-// of the average frequency, this way one observer can handle both general
-// systems and networks.
-template <typename mean_field_type>
-class MeanFieldHelper {
-  public:
-
-    std::vector<double> GetMeanFieldPhase(const mean_field_type& mean_field) {
-      std::vector<double> phase;
-      for (size_t i = 1; i < mean_field.size(); ++i) {
-        phase.push_back(mean_field[i]);
-      }
-      return phase;
-    }
-};
-
-
-// Template specialization for networks.
-template<> std::vector<double> MeanFieldHelper<std::vector<std::vector<double> > >
-::GetMeanFieldPhase(const std::vector<std::vector<double> >& mean_field) {
-  std::vector<double> phase;
-  for (unsigned int i = 0; i < mean_field.size(); ++i) {
-    for (size_t j = 1; j < mean_field[i].size(); ++j) {
-      phase.push_back(mean_field[i][j]);
-    }
-  }
-  return phase;
-}
 
 
 // TODO: Add tests for network
