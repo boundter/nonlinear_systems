3b7d2e01a48505009626ca7bdd982da981e49a23
Erik Teichmann <kontakt.teichmann@gmail.com>
2018-05-03 15:32:00 +0200

    Add docs to statistics

--- include/nonlinear_systems/misc/statistics.hpp
+++ include/nonlinear_systems/misc/statistics.hpp
@@ -19,7 +19,15 @@ state_type SampleDistribution(size_t number_samples,
 }
 
 
-// TODO: check size?
+/*!
+ *  Updates the moving average.
+ *
+ *  @param x the current state.
+ *  @param step_number the number of previous steps/ data points. It will be
+ *  incremented automatically.
+ *  @param average the average of the previous state. It will be overwritten
+ *  with the new average.
+ */
 template<typename state_type=std::vector<double> > 
 void UpdateAverage(const state_type& x, unsigned int& step_number, 
     state_type& average) {
@@ -31,11 +39,27 @@ void UpdateAverage(const state_type& x, unsigned int& step_number,
 
 
 // TODO: chekc size?
+/*!
+ *  Updates the moving average and sum of squares, needed for the variance. The
+ *  variance can be calculated by dividing the sum of squares by the number of
+ *  datapoints - 1, but the sum of squares is numerically more stable than the
+ *  variance.
+ *
+ *  @param x the current state.
+ *  @param step_number the number of tspes/ data points. It will be incremented
+ *  automatically.
+ *  @param average the average of the previous state. It will be overwritten
+ *  with the new average.
+ *  @param sum_of_squares the sum of suqares of the previous state. It will be
+ *  overwritten with the new average.
+ */
 template<typename state_type=std::vector<double> >
 void UpdateSumOfSquares(const state_type& x, unsigned int& step_number,
     state_type& average, state_type& sum_of_squares) {
   state_type previous_average = average;
   UpdateAverage(x, step_number, average);
+  // for the first step there is no sum of squares, this corresponds to
+  // step_number == 2, because it was already incremented in UpdateAverage
   if (step_number == 2) {
     sum_of_squares = state_type(x.size(), 0);
   }
