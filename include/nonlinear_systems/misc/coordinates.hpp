2d00654416db9d645ff9219cfa4f2fd172ca5b9a
Erik Teichmann <kontakt.teichmann@gmail.com>
2018-05-10 11:52:22 +0200

    Overload CartesianToSpherical to accept iterators

--- include/nonlinear_systems/misc/coordinates.hpp
+++ include/nonlinear_systems/misc/coordinates.hpp
@@ -13,32 +13,46 @@ namespace nonlinear_systems {
  * coordinates with polar angle and azimuth!
  */
 template<typename state_type>
-state_type CartesianToSpherical(const state_type& cartesian) {
-  unsigned int dimension = cartesian.size();
+state_type CartesianToSpherical(typename state_type::const_iterator begin,
+    typename state_type::const_iterator end) {
+  unsigned int dimension = end - begin;
   state_type spherical;
   state_type sum_squared(dimension);
-  for (size_t i = 0; i < dimension; ++i) {
-    sum_squared[0] += cartesian[i]*cartesian[i];
+  for (typename state_type::const_iterator i = begin; i != end; ++i) {
+    sum_squared[0] += (*i)*(*i);
   }
   for (size_t i = 1; i < dimension; ++i) {
-    sum_squared[i] = sum_squared[i-1] - cartesian[i-1]*cartesian[i-1];
+    sum_squared[i] = sum_squared[i-1] - (*(begin + i-1))*(*(begin + i-1));
   }
   // radius or distance to the origin
   spherical.push_back(sqrt(sum_squared[0]));
   // phases
   for (size_t i = 0; i < dimension - 2; ++i) {
-    spherical.push_back(acos(cartesian[i]/sqrt(sum_squared[i])));
+    spherical.push_back(acos((*(begin + i))/sqrt(sum_squared[i])));
   }
   // last phase is in the range -pi/2 to pi/2
-  if (cartesian.back() < 0) {
-    spherical.push_back(-acos(cartesian[dimension-2]
+  if ((*(end-1)) < 0) {
+    spherical.push_back(-acos((*(begin + dimension-2))
                               /sqrt(sum_squared[dimension-2])));
   }
   else {
-    spherical.push_back(acos(cartesian[dimension-2]
+    spherical.push_back(acos((*(begin + dimension-2))
                              /sqrt(sum_squared[dimension-2])));
   }
   return spherical; 
+}
+
+
+/*!
+ * Calculates the coordinates on a sphere of the same dimension as
+ * the phase space. If the dimension is 1, the corrdinates will be wrapped
+ * around the unit circle. The first coordinate is the radius and the later
+ * ones are the phases. Careful: in 3-d this is not the same as spherical
+ * coordinates with polar angle and azimuth!
+ */
+template<typename state_type>
+state_type CartesianToSpherical(const state_type& cartesian) {
+  return CartesianToSpherical<state_type>(cartesian.begin(), cartesian.end());
 }
 } // nonlinear_systems
 
--- test/test_coordinates.cpp
+++ test/test_coordinates.cpp
@@ -1,5 +1,6 @@
 #define BOOST_TEST_MODULE Coordinates
 #include <boost/test/included/unit_test.hpp>
+#include <cmath>
 #include <vector>
 #include <nonlinear_systems/misc/coordinates.hpp>
 
@@ -27,4 +28,22 @@ BOOST_AUTO_TEST_CASE(CartesianToSpherical3D) {
   for (size_t i = 0; i < spherical.size(); ++i) {
     BOOST_CHECK_CLOSE(spherical[i], analytical[i], 0.1);
   }
+}
+
+
+BOOST_AUTO_TEST_CASE(cartesian_to_spherical_interators) {
+  // states of different dimension (2, 3); (6, 2, -1) 
+  state_type x = {2, 3, 6, 2, -1};
+  state_type analytical_2 = {3.606, 0.9828};
+  state_type analytical_3 = {6.403, 0.3567, -0.4636};
+  state_type spherical_2 = CartesianToSpherical<state_type>(x.begin(), x.begin()+2);
+  BOOST_REQUIRE_EQUAL(spherical_2.size(), analytical_2.size());
+  BOOST_CHECK_CLOSE(spherical_2[0], analytical_2[0], 0.1);
+  BOOST_CHECK_CLOSE(spherical_2[1], analytical_2[1], 0.1);
+  state_type spherical_3 = CartesianToSpherical<state_type>(x.begin()+2, x.end());
+  BOOST_REQUIRE_EQUAL(spherical_3.size(), analytical_3.size());
+  BOOST_CHECK_CLOSE(spherical_3[0], analytical_3[0], 0.1);
+  BOOST_CHECK_CLOSE(spherical_3[1], analytical_3[1], 0.1);
+  BOOST_CHECK_CLOSE(spherical_3[2], analytical_3[2], 0.1);
+  
 }
