94e8cdaa5f8ed5ff4c6873a72459c17c36fe82b0
Erik Teichmann <kontakt.teichmann@gmail.com>
2018-05-18 11:38:53 +0200

    Add MeanField and MeanFieldAndPhase observer to InstantFrequency (untested)

--- include/nonlinear_systems/observers/instantaneous_frequency_observer.hpp
+++ include/nonlinear_systems/observers/instantaneous_frequency_observer.hpp
@@ -6,6 +6,8 @@
 #include <nonlinear_systems/misc/frequency_helper.hpp>
 #include <nonlinear_systems/misc/helper.hpp>
 
+typedef std::vector<double> frequency_type;
+
 namespace nonlinear_systems {
 template<typename system_type, typename state_type = std::vector<double>>
 /*!
@@ -14,7 +16,7 @@ template<typename system_type, typename state_type = std::vector<double>>
  */
 class InstantaneousFrequencyObserver {
   public:
-    std::vector<state_type>& _instantaneous_frequency;
+    std::vector<frequency_type>& _instantaneous_frequency;
     std::vector<double>& _t;
     system_type& _system;
 
@@ -34,16 +36,16 @@ class InstantaneousFrequencyObserver {
      *  @param t the observed time.
      */
     InstantaneousFrequencyObserver(system_type& system,
-      const state_type& one_step_before, double t_before, 
-      const state_type& two_steps_before, double dt , unsigned int dimension,
-      std::vector<state_type>& instantaneous_frequency, std::vector<double>& t)
+      const frequency_type& one_step_before, double t_before, 
+      const frequency_type& two_steps_before, double dt , unsigned int dimension,
+      std::vector<frequency_type>& instantaneous_frequency, std::vector<double>& t)
     : _instantaneous_frequency(instantaneous_frequency), _t(t), _system(system){
       _d = dimension;
       _t_before = t_before;
       double modulo = 2*M_PI;
       double limit_step_size = 1.;
-      _frequency_helper = std::shared_ptr<FrequencyHelper<state_type>>(
-          new FrequencyHelper<state_type>(one_step_before, two_steps_before, dt, 
+      _frequency_helper = std::shared_ptr<FrequencyHelper<frequency_type>>(
+          new FrequencyHelper<frequency_type>(one_step_before, two_steps_before, dt, 
             modulo, limit_step_size));
     }
 
@@ -60,7 +62,7 @@ class InstantaneousFrequencyObserver {
      *  @param t the observed time.
      */
     InstantaneousFrequencyObserver(system_type& system, double dt, 
-        unsigned int dimension, std::vector<state_type>& instantaneous_frequency,
+        unsigned int dimension, std::vector<frequency_type>& instantaneous_frequency,
         std::vector<double>& t) 
     : _instantaneous_frequency(instantaneous_frequency), _t(t), _system(system){
       SetInitialState(dt, dimension);
@@ -68,7 +70,7 @@ class InstantaneousFrequencyObserver {
 
 
     void operator()(const state_type& x, double t) {
-      state_type phases = this->GetPhases();
+      frequency_type phases = this->GetPhases();
       _instantaneous_frequency.push_back(_frequency_helper->operator()(phases));
       _t.push_back(_t_before);
       _t_before = t;
@@ -77,10 +79,10 @@ class InstantaneousFrequencyObserver {
   protected:
     double _t_before;
     unsigned int _d;
-    std::shared_ptr<FrequencyHelper<state_type>> _frequency_helper;
+    std::shared_ptr<FrequencyHelper<frequency_type>> _frequency_helper;
     
-    /*
-     *  ! Minimal constructor for use in derived classes.
+    /*! 
+     *  \brief Minimal constructor for use in derived classes.
      *
      *  This constructor does not properly initialize the Observer. It is used
      *  to do the bare minimum for derived classes. This solves the problem of
@@ -94,13 +96,13 @@ class InstantaneousFrequencyObserver {
      *  @param t the observed time.
      */
     InstantaneousFrequencyObserver(system_type& system, 
-        std::vector<state_type>& instantaneous_frequency, std::vector<double>& t)
+        std::vector<frequency_type>& instantaneous_frequency, std::vector<double>& t)
     : _instantaneous_frequency(instantaneous_frequency), _t(t), _system(system)
     {}
 
     
-    /*
-     *  ! Does the rest of the worker of the miniaml constructor.
+    /*!
+     *  \brief Does the rest of the worker of the miniaml constructor.
      *
      *  This method should be called after the minimal constructor to properly
      *  set the initial conditions of the observer. The reason for this is
@@ -112,25 +114,27 @@ class InstantaneousFrequencyObserver {
      */
     void SetInitialState(double dt, unsigned int dimension) {
       _d = dimension;
-      state_type two_steps_before = this->GetPhases();
+      frequency_type two_steps_before = this->GetPhases();
       _system.Integrate(dt, 1);
-      state_type one_step_before = this->GetPhases();
+      frequency_type one_step_before = this->GetPhases();
       _t_before = _system.GetTime();
       _system.Integrate(dt, 1);
       double modulo = 2*M_PI;
       double limit_step_size = 1.;
-      _frequency_helper = std::shared_ptr<FrequencyHelper<state_type>>(
-          new FrequencyHelper<state_type>(one_step_before, two_steps_before, dt, 
+      _frequency_helper = std::shared_ptr<FrequencyHelper<frequency_type>>(
+          new FrequencyHelper<frequency_type>(one_step_before, two_steps_before, dt, 
             modulo, limit_step_size));
     }
 
 
     // filters the order parameter from the state
-    virtual state_type GetPhases() {
+    virtual frequency_type GetPhases() {
       state_type x = _system.GetPositionSpherical();
-      state_type phases;
+      frequency_type phases;
       if (_d == 1) {
-        phases = x;
+        for (size_t i = 0; i < x.size(); ++i) {
+          phases.push_back(x[i]);
+        }
       }
       else {
         // TODO: check for correct size
@@ -149,32 +153,107 @@ class InstantaneousFrequencyObserver {
 template<typename system_type, typename state_type = std::vector<double>,
   typename mean_field_type = state_type>
 class InstantaneousFrequencyMeanFieldObserver: 
-  public InstantaneousFrequencyObserver<system_type, state_type> {
+  public InstantaneousFrequencyObserver<system_type, std::vector<double>> {
   public:
 
     InstantaneousFrequencyMeanFieldObserver(system_type& system,
-        double dt, std::vector<state_type>& instantaneous_frequency, 
+        double dt, std::vector<frequency_type>& instantaneous_frequency, 
         std::vector<double>& t)
-    : InstantaneousFrequencyObserver<system_type, state_type>(
+    : InstantaneousFrequencyObserver<system_type, frequency_type>(
         system, instantaneous_frequency, t){ 
       this->SetInitialState(dt, 0);
     } 
 
 
     InstantaneousFrequencyMeanFieldObserver(system_type& system,
-      const state_type& one_step_before, double t_before, 
-      const state_type& two_steps_before, double dt,
-      std::vector<state_type>& instantaneous_frequency, std::vector<double>& t)
-    :InstantaneousFrequencyObserver<system_type, state_type>(
-        system, one_step_before, t_before, two_steps_before, dt, 0,
-        instantaneous_frequency, t) {}
+      const mean_field_type& one_step_before, double t_before, 
+      const mean_field_type& two_steps_before, double dt,
+      std::vector<frequency_type>& instantaneous_frequency, std::vector<double>& t)
+    :InstantaneousFrequencyObserver<system_type, frequency_type>(
+        system, instantaneous_frequency, t) {
+      frequency_type phases_one_before = _mean_field_helper.GetMeanFieldPhase(
+          one_step_before);
+      frequency_type phases_two_before = _mean_field_helper.GetMeanFieldPhase(
+          two_steps_before);
+    }
 
   protected:
     MeanFieldHelper<mean_field_type> _mean_field_helper;
 
-    state_type GetPhases() {
-      state_type mean_field = this->_system.CalculateMeanFieldSpherical();
+    frequency_type GetPhases() {
+      mean_field_type mean_field = this->_system.CalculateMeanFieldSpherical();
       return _mean_field_helper.GetMeanFieldPhase(mean_field);
+    }
+};
+
+
+template<typename system_type, typename state_type = std::vector<double>,
+  typename mean_field_type = state_type>
+class InstantaneousFrequencyMeanFieldAndPhaseObserver {
+
+  InstantaneousFrequencyMeanFieldAndPhaseObserver(system_type& system,
+      double dt, unsigned int dimension, 
+      std::vector<frequency_type>& instantaneous_frequency_phase,
+      std::vector<frequency_type>& instantaneous_frequency_mean_field,
+      std::vector<double> t) {
+    frequency_type system_two_before = GetPhases(system, dimension);
+    frequency_type mean_field_two_before = GetPhasesMeanField(system);
+    system.Integrate(dt, 1);
+    frequency_type system_one_before = GetPhases(system, dimension);
+    frequency_type mean_field_one_before = GetPhasesMeanField(system);
+    double t_before = system.GetTime();
+    system.Integrate(dt, 1);
+    _phase_observer = std::shared_ptr<InstantaneousFrequencyObserver<system_type, 
+                    state_type>>( new InstantaneousFrequencyObserver<system_type,
+                        state_type>(system, system_one_before, t_before,
+                          system_two_before, dt, dimension, 
+                          instantaneous_frequency_phase, t));
+    std::vector<double> t_dummy;
+    _mean_field_observer = std::shared_ptr<InstantaneousFrequencyMeanFieldObserver<
+      system_type, state_type, mean_field_type>>( 
+          new InstantaneousFrequencyMeanFieldObserver<system_type, state_type,
+          mean_field_type>(system, mean_field_one_before, t_before, 
+            mean_field_two_before, dt, instantaneous_frequency_mean_field,
+            t_dummy));
+  }
+
+
+  void operator()(const state_type& x, double t) {
+    _phase_observer->operator()(x, t);
+    _mean_field_observer->operator()(x, t);
+  }
+  
+  protected:
+    std::shared_ptr<InstantaneousFrequencyObserver<system_type, state_type>>
+      _phase_observer;
+    std::shared_ptr<InstantaneousFrequencyMeanFieldObserver<system_type, 
+      state_type, mean_field_type>> _mean_field_observer;
+    MeanFieldHelper<mean_field_type> _mean_field_helper;
+
+    frequency_type GetPhasesMeanField(system_type& system) {
+      mean_field_type mean_field = system.CalculateMeanFieldSpherical();
+      return _mean_field_helper.GetMeanFieldPhase(mean_field);
+    }
+
+
+    frequency_type GetPhases(system_type& system, unsigned int dimension) {
+      state_type x = system.GetPositionSpherical();
+      frequency_type phases;
+      if (dimension == 1) {
+        for (size_t i = 0; i < x.size(); ++i) {
+          phases.push_back(x[i]);
+        }
+      }
+      else {
+        // TODO: check for correct size
+        unsigned int N = x.size()/dimension;
+        for (unsigned int i = 0; i < N; ++i) {
+          for (unsigned int j = i*dimension+1; j < (i+1)*dimension; ++j) {
+            phases.push_back(x[j]);
+          }
+        }
+      }
+      return phases;
     }
 };
 } // nonlinear_systems
